ebf91b31ce7e0620d49e0332b3fa52a07ef02d5d
Bryan Bakotich <bakotibj@gmail.com>
2018-03-03 17:57:55 -0800

    Make search look at all fields
    It was just doing a case sensitive search on the last name field. Now it's
    a better omni search

--- src/backend/csv.py
+++ src/backend/csv.py
@@ -1,5 +1,21 @@
 import csv
+import re
 from AddressEntry import AddressEntry
+
+def removePunctuationAndLowercase(stringWithPunc):
+	return re.sub(r'[^\w]', '', stringWithPunc).lower()
+	
+def getCombinedAddress(addressEntry):
+	addressArray = [
+		addressEntry.firstName,
+		addressEntry.lastName,
+		addressEntry.firstName,
+		addressEntry.street,
+		addressEntry.city,
+		addressEntry.state,
+		addressEntry.zip
+	]
+	return removePunctuationAndLowercase(''.join(addressArray))
 
 class CsvBackend():
 	""" Reads Address data from address_book.csv file """
@@ -9,11 +25,13 @@ class CsvBackend():
 			csvReader = csv.reader(addressFile, delimiter=',', quotechar='"')
 			for row in csvReader:
 				entry = AddressEntry(row)
+				entry.addressString = getCombinedAddress(entry)
 				self.addressEntries.append(entry)
 
 	def search(self, st):
 		results = []
+		searchTermNoPunc = removePunctuationAndLowercase(st)
 		for entry in self.addressEntries:
-			if entry.lastName == st:
+			if searchTermNoPunc in entry.addressString:
 				results.append(entry)
 		return results
