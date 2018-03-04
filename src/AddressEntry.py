368950b7dc4d9c32c06c556a337af609de2e688d
Bryan Bakotich <bakotibj@gmail.com>
2018-03-04 12:14:32 -0800

    Handle smart quotes

--- src/AddressEntry.py
+++ src/AddressEntry.py
@@ -1,9 +1,9 @@
 class AddressEntry():
 	""" Model for an entry in the address book """
-	def __init__(self, csvRow):
-		self.firstName = csvRow[0]
-		self.lastName = csvRow[1]
-		self.street = csvRow[2]
-		self.city = csvRow[3]
-		self.state = csvRow[4]
-		self.zip = csvRow[5]
+	def __init__(self):
+		self.firstName = ""
+		self.lastName = ""
+		self.street = ""
+		self.city = ""
+		self.state = ""
+		self.zip = ""
--- src/backend/csv.py
+++ src/backend/csv.py
@@ -23,17 +23,39 @@ class CsvBackend():
 		self.addressEntries = []
 	
 	def connect(self):
-		with open('address_book.csv', newline='') as addressFile:
-			csvReader = csv.reader(addressFile, delimiter=',', quotechar='"')
-			for row in csvReader:
-				entry = AddressEntry(row)
-				entry.addressString = getCombinedAddress(entry)
-				self.addressEntries.append(entry)
+		addressFile = CsvFileNormalizer("address_book.csv")
+		csvReader = csv.reader(addressFile, delimiter=',', quotechar='"')
+		for row in csvReader:
+			entry = AddressEntry()
+			entry.firstName = row[0]
+			entry.lastName = row[1]
+			entry.street = row[2]
+			entry.city = row[3]
+			entry.state = row[4]
+			entry.zip = row[5]
+			entry.addressString = getCombinedAddress(entry)
+			self.addressEntries.append(entry)
 	
 	def search(self, st):
 		results = []
 		searchTermNoPunc = removePunctuationAndLowercase(st)
 		for entry in self.addressEntries:
 			if searchTermNoPunc in entry.addressString:
+				print("search: " + searchTermNoPunc)
+				print("entry: " + entry.addressString)
 				results.append(entry)
 		return results
+
+class CsvFileNormalizer():
+	def __init__(self, fileName):
+		self.fileReader = open(fileName, 'r', newline='')
+		
+	def __iter__(self):
+		return self
+		
+	def __next__(self):
+		line = self.fileReader.readline()
+		if line == '':
+			self.fileReader.close()
+			raise StopIteration
+		return line.replace("“", "\"")
