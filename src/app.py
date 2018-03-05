8c30bd921688db26e10db483b0c01a27ec335fb6
Bryan Bakotich <bakotibj@gmail.com>
2018-03-05 07:15:48 -0800

    Add comments

--- src/app.py
+++ src/app.py
@@ -2,6 +2,10 @@ from cement.core.foundation import CementApp
 from AddressEntry import AddressEntry
 from backend.csv import CsvBackend
 
+# Get an instance of the backend and connect
+# This would be better if it was an HTTPClient making
+# queries to the server component rather than being coupled
+# with the server code
 db = CsvBackend()
 db.connect()
 
@@ -15,11 +19,14 @@ def printResults(results):
 with CementApp("addressbook") as app:
 	app.setup()
 	
+	# define command line args the app will accept
 	app.args.add_argument("-i", "--interactive", action="store_true", dest="interactive", help="interactive mode")
 	app.args.add_argument("extra_arguments", action="store", nargs="*")
 	
 	app.run()
 	
+	# interactive mode. prompt user for input in a loop until
+	# we receive the 'exit' command
 	if app.pargs.interactive:
 		print("Running addressbook lookup in interactive mode")
 		print("Type 'exit' to exit the program\n")
--- src/backend/csv.py
+++ src/backend/csv.py
@@ -3,7 +3,9 @@ import re
 from AddressEntry import AddressEntry
 
 def removePunctuationAndLowercase(stringWithPunc):
+	# first get rid of all non word/space chars
 	noPunc = re.sub(r"[^\w\s]", "", stringWithPunc)
+	# collapse consecutive spaces into a single space
 	collapseSpaces = re.sub(r"\s\s+", " ", noPunc)
 	return collapseSpaces.lower().strip()
 	
@@ -26,14 +28,16 @@ def populateEntryFromCsvRow(entry, row):
 	entry.city = row[3]
 	entry.state = row[4]
 	entry.zip = row[5]
+	# This is the string we'll be searching on
 	entry.addressString = getCombinedAddress(entry)
 
 class CsvBackend():
-	""" Reads Address data from address_book.csv file """
+	""" Reads Address data from address_book.csv file and makes it searchable """
 	def __init__(self):
 		self.addressEntries = []
 	
 	def connect(self):
+		# parse the csv file
 		addressFile = CsvFileNormalizer("address_book.csv")
 		csvReader = csv.reader(addressFile, delimiter=',', quotechar='"')
 		for row in csvReader:
@@ -50,6 +54,7 @@ class CsvBackend():
 		return results
 
 class CsvFileNormalizer():
+	""" Iterator that preprocesses lines in the csv file to ensure consistent quotes """
 	def __init__(self, fileName):
 		self.fileReader = open(fileName, "r", newline="")
 		
